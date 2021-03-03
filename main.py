cbd996392d69696bb43e156a5eb0433a3879d571
Vikvass <76476616+Vikvass@users.noreply.github.com>
2021-03-03 14:11:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-from telegram.ext import Updater, MessageHandler, Filters
-
+from telegram import Update
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 
 TOKEN = '1630917550:AAHGRtXdVE9j5WANyEulzqOEn4aZY6DqV5A'
+
+
+def main():
+    updater = Updater(token=TOKEN)    # объект, который ловит сообщения из телеграм
+
+    dispatcher = updater.dispatcher
+
+    handler = MessageHandler(Filters.all, do_echo)   # отфильтровываем сообщения: теперь устройство должно реагировать
+    start_handler = CommandHandler('start',do_start)
+    help_handler = CommandHandler('help',do_help)
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(handler)
+
+    updater.start_polling()
+    updater.idle()
+    updater.help_polling()
+
+def do_echo(update, context):
+    update.message.reply_text(text='ААААА!')
+
+def do_start(update, context):
+    update.message.reply_text(text="Я - бот") #  ответ на сообщение
+
+def do_help(update: Update, context):
+    user_id = update.message.from_user.id
+    name = update.message.from_user.first_name
+    update.message.reply_text(text=f'Привет, {name}!\nТвой user_id: {user_id}.\nЧем тебе помочь?')
+
+main()
